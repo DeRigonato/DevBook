52e97eb36479c80dde96efe09e2f34349c387484
André Gotardo <al.g.regonato@gmail.com>
2025-04-26 23:00:41 -0300

    Criado validação de email e etapa de hash de senha

--- src/modelos/Usuarios.go
+++ src/modelos/Usuarios.go
@@ -1,55 +1,72 @@
 package modelos
 
 import (
+	"api/src/seguranca"
 	"errors"
 	"strings"
 	"time"
+
+	"github.com/badoux/checkmail"
 )
 
-//Usuario representa um user usando a rede social
-type Usuario struct{
-	ID uint64 `json:"id,omitempty"` //omit empty não retorna valor vazio
-	Nome string `json:"nome,omitempty"`
-	Nick string `json:"nick,omitempty"`
-	Email string `json:"email,omitempty"`
-	Senha string `json:"senha,omitempty"`
+// Usuario representa um user usando a rede social
+type Usuario struct {
+	ID       uint64    `json:"id,omitempty"` //omit empty não retorna valor vazio
+	Nome     string    `json:"nome,omitempty"`
+	Nick     string    `json:"nick,omitempty"`
+	Email    string    `json:"email,omitempty"`
+	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"criadoEM,omitempty"`
 }
 
-func (usuario *Usuario) Preparar(etapa string) error{
-	if err := usuario.validar(etapa); err != nil{
+func (usuario *Usuario) Preparar(etapa string) error {
+	if err := usuario.validar(etapa); err != nil {
 		return err
 	}
 
-	usuario.formatar()
+	if err := usuario.formatar(etapa); err != nil {
+		return err
+	}
 	return nil
 
 }
 
-func (usuario *Usuario) validar(etapa string) error{
-	if usuario.Nome == ""{
+func (usuario *Usuario) validar(etapa string) error {
+	if usuario.Nome == "" {
 		return errors.New("O campo nome não pode estar em branco")
 	}
 
-	if usuario.Nick == ""{
+	if usuario.Nick == "" {
 		return errors.New("O campo nick não pode estar em branco")
 	}
 
-	if usuario.Email == ""{
+	if usuario.Email == "" {
 		return errors.New("O campo email não pode estar em branco")
 	}
 
-	if etapa == "cadastro" && usuario.Senha == ""{
+	if err := checkmail.ValidateFormat(usuario.Email); err != nil {
+		return errors.New("O e-mail inserido é inválido")
+	}
+
+	if etapa == "cadastro" && usuario.Senha == "" {
 		return errors.New("O campo senha não pode estar em branco")
 	}
 
 	return nil
 }
 
-func (usuario *Usuario) formatar(){
+func (usuario *Usuario) formatar(etapa string) error {
 	usuario.Nome = strings.TrimSpace(usuario.Nome)
 	usuario.Nick = strings.TrimSpace(usuario.Nick)
 	usuario.Email = strings.TrimSpace(usuario.Email)
+
+	if etapa == "cadastro" {
+		senhaComHash, err := seguranca.Hash(usuario.Senha)
+		if err != nil {
+			return err
+		}
+
+		usuario.Senha = string(senhaComHash)
+	}
+	return nil
 }
-
-
